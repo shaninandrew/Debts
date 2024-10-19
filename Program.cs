4036605edec131528bdbf410aede363e55faa463
Шанин Андрей <hedgestandart@gmail.com>
2024-10-19 16:12:46 +0300

    ok

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Debts
 {
@@ -24,7 +25,7 @@ namespace Debts
             //Платежи сначало идут не в полном объеме, а потом 
             List<XPaymentItem> paymentItems = new List<XPaymentItem> ();
             paymentItems.Add (new XPaymentItem (50, DateTime.Now.AddMonths (-5).AddDays (10)));
-           // paymentItems.Add (new XPaymentItem (2000, DateTime.Now.AddMonths (-4).AddDays (10)));
+            //paymentItems.Add (new XPaymentItem (2000, DateTime.Now.AddMonths (-4).AddDays (10)));
 
             paymentItems.Add (new XPaymentItem (200, DateTime.Now.AddMonths (-3).AddDays (10)));
             paymentItems.Add (new XPaymentItem (50, DateTime.Now.AddMonths (-2).AddDays (10)));
@@ -32,31 +33,30 @@ namespace Debts
             paymentItems.Add (new XPaymentItem (150, DateTime.Now.AddMonths (-8).AddDays (10)));
             paymentItems.Add (new XPaymentItem (50, DateTime.Now.AddMonths (-7).AddDays (10)));
 
+            Stopwatch sw = Stopwatch.StartNew ();
+            XDebtProcessor processor = new XDebtProcessor (accrualItems, paymentItems, DateTime.Now, 0);
+            sw.Stop ();
 
-            XProcessor processor = new XProcessor (accrualItems, paymentItems, DateTime.Now, 0);
+            Console.WriteLine ($"Обработано за "+sw.ElapsedMilliseconds+" мс");
 
 
+            Console.WriteLine ("Начисления  ___________________");
+            foreach (XAccrualItem ai in accrualItems)
+                Console.WriteLine (ai.ToString ());
 
             Console.WriteLine ("Платежи  ______________________");
             foreach (XPaymentItem pi in paymentItems)
                 Console.WriteLine (pi.ToString ());
 
-
-
             Console.WriteLine ("Итоги  ______________________");
             foreach (XDebtItem di in processor.Debts)
-            { 
                 Console.WriteLine (di.ToString ());
-            }
 
-            Console.WriteLine ("Остаток сальдо после последнего погашения задолженности платежом: "+processor.CurrentSaldo.ToString("0.##"));
+            Console.WriteLine ("Остаток сальдо после последнего погашения задолженности платежом: "+processor.CurrentSaldo.ToString ("0.##"));
 
-            Console.WriteLine ("Итоги для расчета пени ______");
-            foreach (XDebtItem di in processor.PostProcess())
-            {
+            Console.WriteLine ($"\r\n\r\nИтоги для расчета пени ______ Остаток долга = {processor.CurrentSaldo.ToString("0.##")} р"); 
+            foreach (XDebtItem di in processor.PostProcess (!true))
                 Console.WriteLine (di.ToString ());
-            }
-
         }
     }
 
